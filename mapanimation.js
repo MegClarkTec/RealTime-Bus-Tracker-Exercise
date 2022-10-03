fe2e60459747686dd82cf0e00a5568f16d3de5f8
MegClarkTec <meganclark.tec@gmail.com>
2022-10-03 12:01:44 -0700

    Add files via upload

---
+++ mapanimation.js
@@ -0,0 +1,12 @@
+function move() {
+    setTimeout(() => {
+      if (counter >= busStops.length) return;
+      marker.setLngLat(busStops[counter]);
+      counter++;
+      move();
+    }, 1000);
+  }
+  
+  if (typeof module !== 'undefined') {
+    module.exports = { move };
+  }
